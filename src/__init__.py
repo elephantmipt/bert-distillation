d226d072d69dc7721261059dac83a7f0b2fceeec
root <balaganskij.nn@phystech.edu>
2020-05-01 08:40:46 +0000

    add ranger optimizer to init

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from catalyst.dl import registry
+from torch_optimizer import Ranger
+
+from .experiment import Experiment  # noqa: F401
+from .models import BertForMLM, DistilbertStudentModel
+from .runners import DistilMLMRunner as Runner  # noqa: F401
+
+registry.Model(BertForMLM)
+registry.Model(DistilbertStudentModel)
+
+registry.Optimizer(Ranger)
